47c8a973f50cd1f6d02ac8d739c710e7e753ecf0
unknown <enginoobz@gmail.com>
2021-08-17 19:18:47 +0200

    Skeleton layout

--- index.php
+++ index.php
@@ -10,11 +10,24 @@ Head('trillo')
         ->show();
 ?>
 
-<body class="flat-style ">
-        <div class="display-table">
+<body class="flat-style custom-bg">
+        <!-- <div class="display-table">
                 <div class="display-content ">
                         <section class="container">
                         </section>
+                </div>
+        </div> -->
+        <div class="content-container">
+                <header class="header">
+                        Header
+                </header>
+                <div class="content">
+                        <nav class="sidebar">
+                                Nav
+                        </nav>
+                        <main class="hotel-view">
+                                View
+                        </main>
                 </div>
         </div>
         <script async src="../../dui/js/main.js" type="module"></script>
