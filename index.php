2175a090441d27f9b60b4fd9f0d2564e34d7862a
unknown <enginoobz@gmail.com>
2021-08-17 17:36:14 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+
+<?php
+include_once __DIR__ . "/../../dui/_index.php";
+
+Head('trillo')
+        ->meta(author: "enginoobz", description: "landing page for trillo")
+        ->css('css/main.css')
+        ->show();
+?>
+
+<body class="flat-style ">
+        <div class="display-table">
+                <div class="display-content ">
+                        <section class="container">
+                        </section>
+                </div>
+        </div>
+        <script async src="../../dui/js/main.js" type="module"></script>
+</body>
+
+</html>
