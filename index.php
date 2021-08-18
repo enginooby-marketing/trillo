9eae5cdba61260e240de673107a8cd34c986bc27
unknown <enginoobz@gmail.com>
2021-08-18 17:48:29 +0200

    [Header] User nav

--- index.php
+++ index.php
@@ -33,6 +33,24 @@ Head('trillo')
                                 </button>
                         </form>
 
+                        <nav class="user-nav">
+                                <div class="user-nav__icon-box">
+                                        <svg class="user-nav__icon">
+                                                <use href="img/sprite.svg#icon-bookmark"></use>
+                                        </svg>
+                                        <span class="user-nav__notification">7</span>
+                                </div>
+                                <div class="user-nav__icon-box">
+                                        <svg class="user-nav__icon">
+                                                <use href="img/sprite.svg#icon-chat"></use>
+                                        </svg>
+                                        <span class="user-nav__notification">13</span>
+                                </div>
+                                <div class="user-nav__user">
+                                        <img src="img/user.jpg" alt="User photo" class="user-nav__user-photo">
+                                        <span class="user-nav__username">enginoobz</span>
+                                </div>
+                        </nav>
 
                 </header>
                 <!-- HEADER END -->
