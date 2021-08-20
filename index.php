7460b5f197e994c16890ac0a4e559b8298104814
unknown <enginoobz@gmail.com>
2021-08-20 18:34:27 +0200

    [Content] Overview

--- index.php
+++ index.php
@@ -86,6 +86,35 @@ Head('trillo')
           </figure>
         </div>
         <!-- OVERVIEW -->
+        <div class="overview">
+          <h1 class="overview__heading">
+            Hotel Las Palmas
+          </h1>
+          <div class="overview__stars">
+            <svg class="overview__icon-star">
+              <use href="img/sprite.svg#icon-star"></use>
+            </svg>
+            <svg class="overview__icon-star">
+              <use href="img/sprite.svg#icon-star"></use>
+            </svg>
+            <svg class="overview__icon-star">
+              <use href="img/sprite.svg#icon-star"></use>
+            </svg>
+            <svg class="overview__icon-star">
+              <use href="img/sprite.svg#icon-star"></use>
+            </svg>
+          </div>
+          <div class="overview__location">
+            <svg class="overview__icon-location">
+              <use href="img/sprite.svg#icon-location-pin"></use>
+            </svg>
+            <button class="btn-inline">Albufeira, Portugal</button>
+          </div>
+          <div class="overview__rating">
+            <div class="overview__rating-average">6.9</div>
+            <div class="overview__rating-count">429 views</div>
+          </div>
+        </div>
       </main>
     </div>
   </div>
